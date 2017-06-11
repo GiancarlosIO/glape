266672014bbd2ee2fabcfa54bdea4cfe7e26d878
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-06-11 12:41:43 -0500

    Scroll to intro-section with arrowButton in landing page

--- app/javascript/app/landing.js
+++ app/javascript/app/landing.js
@@ -1,1 +1,13 @@
-console.log('code for landing');
+// Landing code
+import Rx from 'rxjs';
+import scrollTo from './utils/scrollTo';
+
+// scroll to second section
+document.addEventListener('DOMContentLoaded', () => {
+  const btnArrow = document.querySelector('#btnArrow');
+  const btnArrowObs = Rx.Observable.fromEvent(btnArrow, 'click');
+  btnArrowObs.subscribe((e) => {
+    console.log('clicked arrow');
+    scrollTo('#introSection', 1000, -100);
+  });
+});
