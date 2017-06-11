41e491fd7c5ddea75ea81ac0d74825bbbddb32b7
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-06-11 12:50:05 -0500

    Scroll top to click in logo

--- app/javascript/app/landing.js
+++ app/javascript/app/landing.js
@@ -5,9 +5,11 @@ import scrollTo from './utils/scrollTo';
 // scroll to second section
 document.addEventListener('DOMContentLoaded', () => {
   const btnArrow = document.querySelector('#btnArrow');
+  const headerLogo = document.querySelector('#headerLogo');
+  const headerLogoOBs = Rx.Observable.fromEvent(headerLogo, 'click');
   const btnArrowObs = Rx.Observable.fromEvent(btnArrow, 'click');
-  btnArrowObs.subscribe((e) => {
-    console.log('clicked arrow');
-    scrollTo('#introSection', 1000, -100);
-  });
+  // scroll to intro section
+  btnArrowObs.subscribe((e) => scrollTo('#introSection', 1000, -100));
+  // scroll top
+  headerLogoOBs.subscribe((e) => scrollTo('body', 1000));
 });
