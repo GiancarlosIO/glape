94bd378ac86d5b14ec45e8195768bd76011a9542
Fabian Peña <f.pena.jacobo@gmail.com>
2017-07-15 16:51:37 -0500

    ADD: refresh data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Participation.delete_all; Project.delete_all; Company.delete_all; User.delete_all; UserType.delete_all
