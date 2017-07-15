d09e261411e8abbdeb438affe1eb3ddb5c05c143
Fabian Peña <f.pena.jacobo@gmail.com>
2017-07-15 17:06:55 -0500

    ADD: user companies rows

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,10 @@ UserType.create(name: 'Institución', code: 'company')
 UserType.create(name: 'Especialista', code: 'leader')
 UserType.create(name: 'Developer', code: 'dev')
 
-User.create(email: '')
+u = User.create(email: 'demuna@goreica.org', password: '123123123', user_type: UserType.find_by_code('company'))
+puts u.errors.messages
+Company.create(user: User.find_by_email('demuna@goreica.org'), name: 'Demuna de Ica')
+User.create(email: 'hospital@goreica.org', password: '123123123', user_type: UserType.find_by_code('company'))
+Company.create(user: User.find_by_email('hospital@goreica.org'), name: 'Hospital de Ica')
+User.create(email: 'colegio@goreica.org', password: '123123123', user_type: UserType.find_by_code('company'))
+Company.create(user: User.find_by_email('colegio@goreica.org'), name: 'Colegio de Ica')
