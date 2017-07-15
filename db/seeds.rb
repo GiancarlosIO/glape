47243d5956e5bca6fedb619943b43f335cc1acd8
Fabian Peña <f.pena.jacobo@gmail.com>
2017-07-15 16:58:53 -0500

    ADD: usertype rows

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
 Participation.delete_all; Project.delete_all; Company.delete_all; User.delete_all; UserType.delete_all
+
+UserType.create(name: 'Institución', code: 'company')
+UserType.create(name: 'Especialista', code: 'leader')
+UserType.create(name: 'Developer', code: 'dev')
